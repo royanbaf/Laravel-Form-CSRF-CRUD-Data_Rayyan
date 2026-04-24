381edb722ffe47bfd170a625096aa08feec57b99
Rayyan Hasan Bafadal <royan@MacBook-Air-Rayyan.local>
2026-04-24 10:09:35 +0700

    Update CRUD dan proteksi CSRF

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FirstController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\StoreController;
 use Illuminate\Support\Facades\Route;
 use Termwind\Components\Raw;
@@ -62,3 +63,11 @@ Route::patch('/update-cart/{product_id}', [StoreController::class, 'update_cart'
 Route::delete('/remove-from-cart/{product_id}', [StoreController::class, 'remove_from_cart'])->name('remove_from_cart');
 
 Route::post('/checkout', [StoreController::class, 'checkout'])->name('checkout');
+
+Route::get('/payment/return/{order_id}', [StoreController::class, 'payment_return'])->name('payment_return');
+
+Route::get('/payment/status/{order_id}', [StoreController::class, 'payment_status'])->name('payment_status');
+
+Route::get('/orders',[OrderController::class, 'index'])->name('orders');
+
+Route::get('/order/{order_id}', [OrderController::class, 'order_details'])->name('order_details');
